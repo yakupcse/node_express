2d8d1296470b02495cc4bceb027379178b054464
yakup.kaya@hepsiburada.com <Yakup005>
2020-05-17 23:44:57 +0300

    print hostname

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var os = require('os');
+
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express', hostname: os.hostname()});
 });
 
 module.exports = router;
